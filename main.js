03dff54aafb3a299460de5b93ddfec987df32029
unknown <brunorios314@gmail.com>
2022-05-25 12:14:36 -0300

    tercer subida

--- main.js
+++ main.js
@@ -24,12 +24,9 @@
    ];
 
 
-   productos.map(function(productos) {
-     
-     console.log(productos.nombre+" => "+productos.precio);
-    })
 
-    const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+
+  const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
 
 for (const producto of productos) {
     guardarLocal(producto.id, JSON.stringify(producto));
@@ -44,7 +41,7 @@ fetch ("/productos.Json")
 .then(response => response.json())
 .then(data => {
   data.forEach((producto) => {
-    const li = document.createElement("li");
+/*    const li = document.createElement("li");
     li.innerHTML = `
               <h4>${producto.nombre}</h4>
               <p>${producto.precio}</p>
@@ -53,68 +50,16 @@ fetch ("/productos.Json")
               <hr/>
           `;
 
-    lista.append(li);
+    lista.append(li);*/
   });
 }).catch((error) => {
   console.log(error);
 }); 
 
     
-    // formulario//
-       
-  const mensajeRecibido = [];
-    
-  const form = document.getElementById("form").addEventListener("submit", (e)=> {
-  e.preventDefault()
-
-  swal({
-      title: "formulario enviado",
-      text: "Gracias por contactarnos",
-      icon: "success",
-      button: "Aceptar",
-    });
-    
-  const name = document.getElementById("name");
-    
-  const email =document.getElementById("email");
-    
-  const phone = document.getElementById("phone");
-    
-  const pais = document.getElementById("pais");
-    
-  const message = document.getElementById("message");
-    
- 
-  const mensajeEnviado = {
-      nombre: name,
-      emailDirection: email,
-      country: pais,
-      mensaje: message
-      };
-  
-    mensajeRecibido.push(mensajeEnviado)
-      onsole.log(mensajeRecibido);
-    
-    document.getElementById("form").reset();
-
-    localStorage.setItem("mensaje recibido" , JSON.stringify(mensajeEnviado))
-    });
-
-    //aca esto funciona pero el array que me crea al enviar el mensaje no dice lo escrito sino lo que esta en la constante mensajeEnviado
-
-    //modificar dom//
 
 
-   let remeras = document.getElementById("remeras");
 
-   let zapatillas = document.getElementById("Zapatillas");
-
-   console.log(remeras.innerText);
-
-   console.log(zapatillas.innerText);
-
-
-  localStorage.setItem(`arreglos`, JSON.stringify(productos.nombre))
     
 
 
